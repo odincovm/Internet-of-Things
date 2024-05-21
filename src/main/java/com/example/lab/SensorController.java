5f2ab21aae86608cade1605877e3ceed82c31bfb
odincovm <odincovmisa16@gmail.com>
2024-05-21 21:01:20 +0300

    Готово

--- src/main/java/com/example/lab/SensorController.java
+++ src/main/java/com/example/lab/SensorController.java
@@ -11,16 +11,37 @@ import java.util.concurrent.TimeUnit;
 public class SensorController {
     @Autowired
     private SensorRepo Spero;
-    Long time = 5L;
+    Long time = 0L;
     int day=0;
     @PostMapping("/start")
     public ResponseEntity setdata(@RequestBody limitations lim){
         try {
-            while(day<50) {
+            while(day<24) {
                 time++;
-                SensorEntity sens = new SensorEntity(time, lim.min, lim.max);
-                Spero.save(sens);
+                SensorEntity sens = new SensorEntity(time, lim.min, lim.max,"norm");
                 day+=1;
+                if(sens.getTemperature_sensor()>lim.max-2){
+                    lim.min-=2;
+                    lim.max-=2;
+                    sens.setCondition("Down");
+                }
+                if(sens.getTemperature_sensor()<lim.min+2) {
+                    lim.min += 2;
+                    lim.max += 2;
+                    sens.setCondition("Up");
+
+                }
+                if(lim.max>=36) {
+                    lim.max = 22;
+                    lim.min = 12;
+                    sens.setCondition("Max_reset");
+                }
+                if (lim.min < 10) {
+                    lim.max = 22;
+                    lim.min = 12;
+                    sens.setCondition("Min_reset");
+                }
+                Spero.save(sens);
                 TimeUnit.SECONDS.sleep(5);
             }
             return ResponseEntity.ok("Сгенерирована новая запись в бд");
--- src/main/java/com/example/lab/SensorEntity.java
+++ src/main/java/com/example/lab/SensorEntity.java
@@ -1,9 +1,9 @@
 package com.example.lab;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+import java.util.Random;
 
 @Entity
 public class SensorEntity {
@@ -11,24 +11,40 @@ public class SensorEntity {
     @Id
     private Long time;
 
-    private int temperature;
+    private int temperature_sensor;
 
+    private int max;
+    private int min;
 
-    public SensorEntity(Long time,int minimum,int maximum){
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    private String condition;
+
+    public SensorEntity(Long time,int minimum,int maximum,String c){
         // генерация случайного числа
+        Random rand = new Random();
         this.time=time;
-        int randomNum = (int) (minimum + (int)(Math.random() * maximum));
-        temperature = randomNum;
+        int randomNum = rand.ints(minimum,(maximum+1)).findFirst().getAsInt();
+        temperature_sensor = randomNum;
+        max=maximum;
+        min=minimum;
+        condition = c;
     }
     public SensorEntity(){
         time=1L;
     }
-    public void setTemperature(int temperature) {
-        this.temperature = temperature;
+    public void setTemperature_sensor(int temperature_sensor) {
+        this.temperature_sensor = temperature_sensor;
     }
 
-    public int getTemperature() {
-        return temperature;
+    public int getTemperature_sensor() {
+        return temperature_sensor;
     }
 
     public Long getTime() {
