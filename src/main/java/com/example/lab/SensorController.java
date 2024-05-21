9c94dfec10f91f3cf7c19d5eb94837e42753c6ab
odincovm <odincovmisa16@gmail.com>
2024-05-21 20:00:42 +0300

    Сразу генерирует 50 запросов с задержкой в 5 секунд

--- src/main/java/com/example/lab/SensorController.java
+++ src/main/java/com/example/lab/SensorController.java
@@ -4,25 +4,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
-@RequestMapping("/sensors")
+@RequestMapping("/Conditioner")
 public class SensorController {
     @Autowired
     private SensorRepo Spero;
-    Long time = 0L;
-
-    @PostMapping
+    Long time = 5L;
+    int day=0;
+    @PostMapping("/start")
     public ResponseEntity setdata(@RequestBody limitations lim){
-            time++;
-            SensorEntity sens = new SensorEntity(time, lim.min, lim.max);
-            Spero.save(sens);
+        try {
+            while(day<50) {
+                time++;
+                SensorEntity sens = new SensorEntity(time, lim.min, lim.max);
+                Spero.save(sens);
+                day+=1;
+                TimeUnit.SECONDS.sleep(5);
+            }
             return ResponseEntity.ok("Сгенерирована новая запись в бд");
-
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Произошла ошибка");
+        }
     }
-    @GetMapping("/")
+    @GetMapping("/check")
     public ResponseEntity getSensor(){
         try {
-            return ResponseEntity.ok("Сервер работает");
+            return ResponseEntity.ok("Кондиционер подключен и готов работать");
         }catch (Exception e){
             return ResponseEntity.badRequest().body("Произошла ошибка");
         }
